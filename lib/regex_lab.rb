a26c7fe4820305ad31fc15481043867fee8b2599
Wasif Khan <waasif.khaan@gmail.com>
2018-05-04 20:38:38 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ text.match(/^[A-Z].+[.?!]$/)? true : false
 end
 
 def valid_phone_number?(phone)
-phone.match(/\d{10}/)||phone.match(/\(\d{3}\)\d{3}-\d{4}/)|| phone.match(/\d{3}\s\d{3}\s\d{4}/)||phone.match(/ /)? true : false
+phone.match(/\d{10}/)||phone.match(/\(\d{3}\)\d{3}-\d{4}/)|| phone.match(/\d{3}\s\d{3}\s\d{4}/)||phone.match(/\(\d{3}\)\d{7}/)? true : false
 end
